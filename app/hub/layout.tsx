e522b06035928b1cdcbb5727619b9611d030d161
Alexander Popadyuk <al.popadyuk@gmail.com>
2023-12-12 22:33:24 +0100

    fix

--- app/hub/layout.tsx
+++ app/hub/layout.tsx
@@ -14,6 +14,7 @@ const createServerSupabaseClient = cache(() => {
   return createServerComponentClient<Database>({ cookies: () => cookieStore })
 })
 
+// @ts-ignore
 export default async function Layout({
   children,
 }: {
