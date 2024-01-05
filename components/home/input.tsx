8195e5308c8e48426d0301d28415098483c4c0a4
Alexander Popadyuk <al.popadyuk@gmail.com>
2024-01-05 16:03:53 +0100

    fix

--- components/home/input.tsx
+++ components/home/input.tsx
@@ -311,7 +311,9 @@ export const SourceInput = () => {
 
       const successMessage = `Saved ${
         files?.length ? `${files.length} files${!!value ? " and " : ""}` : ""
-      } ${isValidUrl ? "1 link" : value ? "1 text note" : ""} to your knowledge base.`
+      } ${
+        isValidUrl ? "1 link" : value ? "1 text note" : ""
+      } to your knowledge base.`
       setSuccessMessage(successMessage)
 
       setTimeout(() => {
@@ -339,65 +341,58 @@ export const SourceInput = () => {
         <div className="overflow-hidden flex flex-col w-full shadow-md focus-within:shadow-lg min-h-12 bg-slate-50 focus-within:shadow-black/40 rounded-2xl transition-all">
           <div
             style={{
-              height: success ? "60px" : "0px",
+              height: hasFiles || success ? "60px" : "0px",
               transformOrigin: "50% 50% 0px",
             }}
             className={cn(
               "z-0 transition-all duration-300 ease-in-out",
-              success ? "border-b border-slate-200" : ""
+              hasFiles || success ? "border-b border-slate-200" : ""
             )}
           >
-            <div className="flex items-center gap-2.5 p-2.5">
-              <CheckCircle className="text-green-600" size={24} />
-              <div>
-                <h4 className="text-sm font-medium">Success 🎉</h4>
-                <p className="text-xs">{successMessage}</p>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{
-              height: hasFiles ? "60px" : "0px",
-              transformOrigin: "50% 50% 0px",
-            }}
-            className={cn(
-              "z-0 transition-all duration-300 ease-in-out",
-              hasFiles ? "border-b border-slate-200" : ""
-            )}
-          >
-            <div className="flex gap-2 p-2.5">
-              {files?.map((file) => (
-                <div
-                  key={`file-${file.name}-${file.size}`}
-                  className="flex relative items-center gap-2 w-full hover:bg-slate-100 justify-center rounded-md transition-colors group py-0.5 px-1 max-w-[130px]"
-                >
-                  {getFileLogo(file.name)}
-                  <div className="flex flex-col">
-                    <p className="text-sm font-medium truncate max-w-[60px] md:max-w-[90px]">
-                      {file.name}
-                    </p>
-                    <span className="text-xs text-slate-500">
-                      {getFileSize(file.size)}
-                    </span>
+            {hasFiles && (
+              <div className="flex gap-2 p-2.5">
+                {files?.map((file) => (
+                  <div
+                    key={`file-${file.name}-${file.size}`}
+                    className="flex relative items-center gap-2 w-full hover:bg-slate-100 justify-center rounded-md transition-colors group py-0.5 px-1 max-w-[130px]"
+                  >
+                    {getFileLogo(file.name)}
+                    <div className="flex flex-col">
+                      <p className="text-sm font-medium truncate max-w-[60px] md:max-w-[90px]">
+                        {file.name}
+                      </p>
+                      <span className="text-xs text-slate-500">
+                        {getFileSize(file.size)}
+                      </span>
+                    </div>
+                    <TooltipProvider delayDuration={0}>
+                      <Tooltip>
+                        <TooltipTrigger asChild>
+                          <div
+                            onClick={() => handleFileRemove(file)}
+                            className="absolute -top-2.5 -right-2.5 w-5 h-5 justify-center items-center flex sm:group-hover:flex sm:hidden rounded-full bg-white cursor-pointer"
+                          >
+                            <X size={12} />
+                          </div>
+                        </TooltipTrigger>
+                        <TooltipContent side="right" className="bg-slate-50">
+                          Remove file
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
                   </div>
-                  <TooltipProvider delayDuration={0}>
-                    <Tooltip>
-                      <TooltipTrigger asChild>
-                        <div
-                          onClick={() => handleFileRemove(file)}
-                          className="absolute -top-2.5 -right-2.5 w-5 h-5 justify-center items-center flex sm:group-hover:flex sm:hidden rounded-full bg-white cursor-pointer"
-                        >
-                          <X size={12} />
-                        </div>
-                      </TooltipTrigger>
-                      <TooltipContent side="right" className="bg-slate-50">
-                        Remove file
-                      </TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
+                ))}
+              </div>
+            )}
+            {success && (
+              <div className="flex items-center gap-2.5 p-2.5">
+                <CheckCircle className="text-green-600" size={24} />
+                <div>
+                  <h4 className="text-sm font-medium">Success 🎉</h4>
+                  <p className="text-xs">{successMessage}</p>
                 </div>
-              ))}
-            </div>
+              </div>
+            )}
           </div>
           <div className="flex w-full transition-all duration-300 z-10">
             <textarea
