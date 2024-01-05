a2a88f61d7d778e92066cbe24c349c1d068ce9e1
Alexander Popadyuk <al.popadyuk@gmail.com>
2024-01-05 14:18:04 +0100

    fix files display styling

--- components/home/input.tsx
+++ components/home/input.tsx
@@ -115,7 +115,7 @@ export const SourceInput = () => {
   const hasFiles = files && files.length > 0
 
   return (
-    <div className="relative w-full max-w-[90%] sm:max-w-lg m-auto mt-20">
+    <div className="relative w-full max-w-[90%] sm:max-w-lg m-auto mt-24">
       <div className="w-full absolute bottom-0 left-0">
         <div className="overflow-hidden flex flex-col w-full shadow-md focus-within:shadow-lg divide-slate-600 min-h-12 bg-slate-50 focus-within:shadow-black/40 rounded-2xl transition-all">
           <div
@@ -124,18 +124,19 @@ export const SourceInput = () => {
               transformOrigin: "50% 50% 0px",
             }}
             className={cn(
-              "z-0 grid grid-cols-1 sm:grid-cols-3 gap-x-4 gap-y-2 transition-all duration-300 ease-in-out",
+              "z-0 transition-all duration-300 ease-in-out",
               hasFiles ? "border-b" : ""
             )}
           >
+            <div className="flex gap-2 p-2.5">
             {files?.map((file) => (
               <div
                 key={`file-${file.name}-${file.size}`}
-                className="flex relative items-center gap-2 w-full hover:bg-slate-100 justify-center rounded-md transition-colors group m-2.5"
+                className="flex relative items-center gap-2 w-full hover:bg-slate-100 justify-center rounded-md transition-colors group py-0.5 px-1 max-w-[130px]"
               >
                 {getFileLogo(file.name)}
                 <div className="flex flex-col">
-                  <p className="text-sm font-medium truncate max-w-[120px]">
+                  <p className="text-sm font-medium truncate max-w-[60px] md:max-w-[90px]">
                     {file.name}
                   </p>
                   <span className="text-xs text-slate-500">
@@ -147,7 +148,7 @@ export const SourceInput = () => {
                     <TooltipTrigger asChild>
                       <div
                         onClick={() => handleFileRemove(file)}
-                        className="absolute -top-2.5 -right-2.5 w-5 h-5 justify-center items-center group-hover:flex hidden rounded-full bg-white cursor-pointer"
+                        className="absolute -top-2.5 -right-2.5 w-5 h-5 justify-center items-center flex sm:group-hover:flex sm:hidden rounded-full bg-white cursor-pointer"
                       >
                         <X size={12} />
                       </div>
@@ -159,6 +160,7 @@ export const SourceInput = () => {
                 </TooltipProvider>
               </div>
             ))}
+            </div>
           </div>
           <div className="flex w-full transition-all duration-300 z-10">
             <textarea
