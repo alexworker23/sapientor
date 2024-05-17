b77db3e26b2281d01821d6dca26ecac827f1026d
Alexander Popadyuk <al.popadyuk@gmail.com>
2024-05-17 17:59:26 +0200

    feat: warning

--- components/landing/hero-banner.tsx
+++ components/landing/hero-banner.tsx
@@ -4,9 +4,13 @@ import { Button } from "../ui/button"
 import { AppPreviewCard } from "./app-preview-card"
 
 export const HeroBanner = () => {
+
   return (
     <div className="w-full">
       <div className="container px-4 md:px-6">
+        <p className="text-yellow-500 text-semibold text-center relative">
+          Project has been shut down and is used for presentation purposes only.
+        </p>
         <AppPreviewCard />
         <div className="mx-auto max-w-2xl flex flex-col items-center">
           <p className="max-w-xl text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400 mb-5 text-center">
